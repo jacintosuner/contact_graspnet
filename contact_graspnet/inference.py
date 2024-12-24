12266e27d796fa6e58fa46862a779f91b0a5a515
Panda <kallolsaharesearch@gmail.com>
2024-12-23 22:30:41 -0500

    latest changes for vtamp

--- contact_graspnet/inference.py
+++ contact_graspnet/inference.py
@@ -56,48 +56,68 @@ def inference(global_config, checkpoint_dir, input_paths, results_path, K=None, 
     os.makedirs(results_path, exist_ok=True)
 
     # Process example test scenes
-    for p in glob.glob(input_paths):
-        print('Loading ', p)
+    # for p in glob.glob(input_paths):
+    # print('Loading ', p)
 
-        pc_segments = {}
-        segmap, rgb, depth, cam_K, pc_full, pc_colors = load_available_input_data(p, K=K)
+    # pc_segments = {}
+    # segmap, rgb, depth, cam_K, pc_full, pc_colors = load_available_input_data(p, K=K)
+    
+    # if segmap is None and (local_regions or filter_grasps):
+    #     raise ValueError('Need segmentation map to extract local regions or filter grasps')
+
+    # if pc_full is None:
+    #     print('Converting depth to point cloud(s)...')
+    #     pc_full, pc_segments, pc_colors = grasp_estimator.extract_point_clouds(depth, cam_K, segmap=segmap, rgb=rgb,
+    #                                                                             skip_border_objects=skip_border_objects, z_range=z_range)
+    
+    initial_pcd = np.load(os.path.join(input_paths, "initial_pcd.npy"))
+    initial_pcd_seg = np.load(os.path.join(input_paths, "initial_pcd_seg.npy"))
+    num_objs = len(np.unique(initial_pcd_seg)) - 1  # background is not an object, so exclude it
+
+    pc_segments = {}
+    for i in range(num_objs):
+        pc_segments[i] = initial_pcd[initial_pcd_seg == i]
+    
+    print('Generating Grasps...')
+    pred_grasps_cam, scores, contact_pts, _ = grasp_estimator.predict_scene_grasps(sess, 
+                                                                                   pc_full = initial_pcd, 
+                                                                                   pc_segments = pc_segments, 
+                                                                                   local_regions=local_regions, 
+                                                                                   filter_grasps=filter_grasps, 
+                                                                                   forward_passes=forward_passes)
+
+    # print(pred_grasps_cam.keys())
+    # print(scores.shape)
+    # print(contact_pts.shape)  
+
+    _ = input('Press any key to continue...')
+
+    # Save results
+    if results_path == 'results':
+        np.savez(os.path.join(results_path, 'predictions_{}'.format(os.path.basename(p.replace('png','npz').replace('npy','npz')))), 
+                    pred_grasps_cam=pred_grasps_cam, scores=scores, contact_pts=contact_pts)
+    else:
+        np.savez(os.path.join(results_path, 'contact_graspnet_results'), 
+                    pred_grasps_cam=pred_grasps_cam, scores=scores, contact_pts=contact_pts)
+
+    # Visualize results
+    # show_image(rgb, segmap)
+    visualize_grasps(initial_pcd, pred_grasps_cam, scores, plot_opencv_cam=True) #, pc_colors=pc_colors)
+    # Visualize the grasp with the highest score
+    best_grasps = {}
+    best_scores = {}
+    for id, score_list in scores.items():
+        max_score_idx = np.argmax(score_list)
+        best_scores[id] = [score_list[max_score_idx]]
+        best_grasps[id] = [pred_grasps_cam[id][max_score_idx]]
+    visualize_grasps(initial_pcd, best_grasps, best_scores, plot_opencv_cam=True) #, pc_colors=pc_colors, visualize_3D_grasp_axis=True)
         
-        if segmap is None and (local_regions or filter_grasps):
-            raise ValueError('Need segmentation map to extract local regions or filter grasps')
-
-        if pc_full is None:
-            print('Converting depth to point cloud(s)...')
-            pc_full, pc_segments, pc_colors = grasp_estimator.extract_point_clouds(depth, cam_K, segmap=segmap, rgb=rgb,
-                                                                                    skip_border_objects=skip_border_objects, z_range=z_range)
-
-        print('Generating Grasps...')
-        pred_grasps_cam, scores, contact_pts, _ = grasp_estimator.predict_scene_grasps(sess, pc_full, pc_segments=pc_segments, 
-                                                                                          local_regions=local_regions, filter_grasps=filter_grasps, forward_passes=forward_passes)  
-
-        # Save results
-        if results_path == 'results':
-            np.savez(os.path.join(results_path, 'predictions_{}'.format(os.path.basename(p.replace('png','npz').replace('npy','npz')))), 
-                      pred_grasps_cam=pred_grasps_cam, scores=scores, contact_pts=contact_pts)
-        else:
-            np.savez(os.path.join(results_path, 'contact_graspnet_results'), 
-                        pred_grasps_cam=pred_grasps_cam, scores=scores, contact_pts=contact_pts)
-
-        # Visualize results
-        show_image(rgb, segmap)
-        visualize_grasps(pc_full, pred_grasps_cam, scores, plot_opencv_cam=True, pc_colors=pc_colors)
-        # Visualize the grasp with the highest score
-        best_grasps = {}
-        best_scores = {}
-        for id, score_list in scores.items():
-            max_score_idx = np.argmax(score_list)
-            best_scores[id] = [score_list[max_score_idx]]
-            best_grasps[id] = [pred_grasps_cam[id][max_score_idx]]
-        visualize_grasps(pc_full, best_grasps, best_scores, plot_opencv_cam=True, pc_colors=pc_colors, visualize_3D_grasp_axis=True)
-        
-    if not glob.glob(input_paths):
-        print('No files found: ', input_paths)
+    # if not glob.glob(input_paths):
+    #     print('No files found: ', input_paths)
         
 if __name__ == "__main__":
+
+    # python3 contact_graspnet/inference.py --np_path initial_pcd.npy --filter_grasps --results_path /home/jacinto/franka-panda-execution/contact_graspnet/contact_graspnet/out --ckpt_dir ~/robot-grasp/third_party/contact_graspnet/checkpoints/scene_test_2048_bs3_hor_sigma_0025
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--ckpt_dir', default='checkpoints/scene_test_2048_bs3_hor_sigma_001', help='Log dir [default: checkpoints/scene_test_2048_bs3_hor_sigma_001]')
